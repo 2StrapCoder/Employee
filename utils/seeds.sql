e1eba205c0861e1e1a68bd71029c89ebeaff9ba0
christopher miguel <chrismiguel115@gmail.com>
2024-02-29 14:06:32 -0600

    added seeds file

---
+++ utils/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO departments (id, name)
+VALUES (1, "Javascript"),
+        (2,"SQL"),
+        (3, "HTML");
+
+INSERT INTO roles (id, title, salary)
+VALUES (1, "Master Programer", 140000),
+        (2, "Intermediate Programer", 100000),
+        (3, "Intern", 50000);
+
+INSERT INTO employees (id, first_name, last_name)
+VALUES (1, "Sandy", "Cheeks"),
+        (2, "Patrick","Star"),
+        (3, "Spongebob", "Squarepants");
